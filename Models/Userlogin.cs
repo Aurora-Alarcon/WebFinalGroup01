b510c18e6ce059e5c8dbd02bd00a0b0cd7c478aa
Sutama Mitra <sutama.mitra@outlook.com>
2020-07-14 12:35:51 -0400

    Contribution from Sutama

--- Models/Userlogin.cs
+++ Models/Userlogin.cs
@@ -5,14 +5,8 @@ namespace WebFinalGroup01.Models
 {
     public class Userlogin
     {
-        ///<summary>
-        /// Gets or sets PersonId.
-        ///</summary>
         public string user { get; set; }
 
-        ///<summary>
-        /// Gets or sets Name.
-        ///</summary>
         public string password { get; set; }
 
     }
